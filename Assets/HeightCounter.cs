30c02ef604677c01414f1f948163fa2391037dd7
Psycho Games <87240832+PsychoGamesTeam@users.noreply.github.com>
2024-07-14 18:31:47 +0400

    Height updated.

--- Assets/HeightCounter.cs
+++ Assets/HeightCounter.cs
@@ -6,17 +6,19 @@ public class HeightCounter : MonoBehaviour
     [SerializeField] private Movement _movement;
     [SerializeField] private TMP_Text _heightText;
     [SerializeField] private string _countString = "{count}";
-    [SerializeField] private float _height = 0f;
+    [SerializeField] private int _height = 0;
+    private float _heightFloat;
     private string _standartHeightString;
     private void Awake()
     {
         _standartHeightString = _heightText.text;
-        _heightText.text = _standartHeightString.Replace(_countString, string.Format("{0:0.000}", _height));
+        _heightText.text = _standartHeightString.Replace(_countString, _height.ToString());
     }
     void Update()
     {
         enabled = _movement.enabled;
-        _heightText.text = _standartHeightString.Replace(_countString, string.Format("{0:0.000}",_height));
-        _height += _movement.verticalSpeed * Time.deltaTime;
+        _heightText.text = _standartHeightString.Replace(_countString, _height.ToString());
+        _heightFloat += (_movement.verticalSpeed * Time.deltaTime);
+        _height = (int)_heightFloat;
     }
 }
