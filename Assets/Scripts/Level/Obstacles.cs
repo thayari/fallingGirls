4df6375992be55ee25ba5841760842564e4f83cb
thayari <50718467+thayari@users.noreply.github.com>
2024-08-25 17:04:01 +0300

    upd: obstacles

--- Assets/Scripts/Level/Obstacles.cs
+++ Assets/Scripts/Level/Obstacles.cs
@@ -1,24 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class ObstacleInfo
+{
+    public GameObject prefab;
+    public int count;
+}
+
+
 public class Obstacles : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float generateDistance = 50f;
+    [SerializeField] private float removeDistance = 10f;
     [SerializeField] private Level level;
-
-    private void Start()
+    [SerializeField] private List<ObstacleInfo> sequence;
+    
+    void Update()
     {
-        level.OnLevelGenerationCompleted += HandleLevelGenerationCompleted;
+        GenerateObstacles();
+        RemoveObstacles();
     }
 
-    private void HandleLevelGenerationCompleted()
+    void GenerateObstacles()
     {
-        float levelHeight = level.TotalLevelHeight;
-        Debug.Log("Total Level Height after generation: " + levelHeight);
+        
     }
 
-    private void OnDestroy()
+    void RemoveObstacles()
     {
-        level.OnLevelGenerationCompleted -= HandleLevelGenerationCompleted;
+        
     }
 }
