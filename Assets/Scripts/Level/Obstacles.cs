aa759f0aa2851c0330d696c7cdb6c35e999e207e
thayari <50718467+thayari@users.noreply.github.com>
2024-12-21 19:40:43 +0300

    upd: obstacles

--- Assets/Scripts/Level/Obstacles.cs
+++ Assets/Scripts/Level/Obstacles.cs
@@ -1,39 +1,44 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
-[System.Serializable]
-public class ObstacleInfo
+public class Obstacle
 {
-    public GameObject prefab;
+    public ObstaclesInfo config;
+    public float positionY;
+    public GameObject container;
+    public GameObject item;
 }
-
 
 public class Obstacles : MonoBehaviour
 {
     [SerializeField] private int obstaclesCount = 100;
     [SerializeField] private float generateDistance = 5f;
     [SerializeField] private float removeDistance = 2f;
-    [SerializeField] private float offset = 100f; // offset from the bottom of a level, where obstacles start to appear
+    [SerializeField] private float offset = 5f; // offset from the bottom of a level, where obstacles start to appear
     [SerializeField] private Level level;
 
     private List<Tuple<Transform, GameObject>> obstacleContainers;
     private Transform playerTransform;
+    private List<Obstacle> obstacles;
 
     private void Awake()
     {
         playerTransform = GameObject.FindWithTag("Player").transform;
         obstacleContainers = new List<Tuple<Transform, GameObject>>();
+        obstacles = new List<Obstacle>();
 
-        level.OnLevelGenerationCompleted += HandleLevelGenerationCompleted;
+
+    level.OnLevelGenerationCompleted += HandleLevelGenerationCompleted;
     }
 
     private void HandleLevelGenerationCompleted()
     {
         Init();
-        Debug.Log("Init obstacles generation");
     }
 
     private void OnDestroy()
@@ -49,11 +54,14 @@ public class Obstacles : MonoBehaviour
 
         if (obstaclesList.Count > 0 && difficultyZonesList.Count > 0)
         {
+            float positionY = offset;
             for (int i = 0; i < difficultyZonesList.Count; i++)
             {
                 DifficultyZonesInfo currentDifficultyZone = difficultyZonesList[i];
+                float distance = currentDifficultyZone.zoneHeight / currentDifficultyZone.obstaclesCount;
 
                 List<ObstaclesInfo> currentObstacles = new List<ObstaclesInfo>();
+
                 foreach (ObstaclesInfo obstacle in obstaclesList)
                 {
                     if (obstacle.difficulty == currentDifficultyZone.difficultyLevel)
@@ -62,69 +70,87 @@ public class Obstacles : MonoBehaviour
                     }
                 }
 
-                Debug.Log("Difficulty " + i);
+                for (int j = 0; j < currentDifficultyZone.obstaclesCount; j++)
+                {
+                    int randomIndex = Random.Range(0, currentObstacles.Count);
+                    obstacles.Add(new Obstacle
+                    {
+                        config = currentObstacles[randomIndex],
+                        positionY = positionY,
+                    });
+                    positionY += distance;
+                }
+
 
             }
-            //float intervalSize = (level.TotalLevelHeight - offset) / obstaclesCount;
 
-            //float currentYPosition = offset;
+            for (int i = 0; i < obstacles.Count; i++)
+            {
+                GameObject container = new GameObject("ObstacleContainer_" + i);
+                Transform containerTransform = container.transform;
 
-            //    for (int i = 0; i < obstaclesCount; i++)
-            //    {
-            //        GameObject container = new GameObject("ObstacleContainer_" + i);
+                containerTransform.SetParent(transform);
+                containerTransform.position = new Vector3(0, obstacles[i].positionY, transform.position.z);
 
-            //        container.transform.SetParent(transform);
-            //        container.transform.position = new Vector3(0, currentYPosition, transform.position.z);
+                obstacles[i].container = container;
 
+                GameObject obstaclePrefab = obstacles[i].config.prefab;
+                GameObject obstacleInstance = Instantiate(obstaclePrefab, containerTransform.position, containerTransform.rotation, containerTransform);
 
-            //        GameObject item = obstaclesList[Random.Range(0, obstaclesList.Count)];
+                obstacles[i].item = obstacleInstance;
 
-            //        obstacleContainers.Add(new Tuple<Transform, GameObject>(container.transform, item));
+                obstacleInstance.transform.eulerAngles = new Vector3(0, Random.Range(0, 2) == 0 ? 180 : 0, 0); // Mirror to left
 
-            //        currentYPosition += intervalSize;
-            //    }
+                obstacleInstance.SetActive(false);
+
+                GetColliderSize(obstacleInstance);
+            }
         }
     }
 
     void Update()
     {
-        //GenerateObstacles();
-        //RemoveObstacles();
+        GenerateObstacles();
+        RemoveObstacles();
     }
+
 
     void GenerateObstacles()
     {
-        foreach (var obstacleData in obstacleContainers)
+        foreach (var obstacle in obstacles)
         {
-            Transform containerTransform = obstacleData.Item1;
-            GameObject obstaclePrefab = obstacleData.Item2;
-
-            if (Vector3.Distance(playerTransform.position, containerTransform.position) <= generateDistance)
+            if (Vector3.Distance(playerTransform.position, obstacle.container.transform.position) <= generateDistance)
             {
-                if (containerTransform.childCount == 0)
-                {
-                    GameObject obstacleInstance = Instantiate(obstaclePrefab, containerTransform.position, containerTransform.rotation, containerTransform);
-
-                    obstacleInstance.transform.eulerAngles = new Vector3(0, Random.Range(0, 2) == 0 ? 180 : 0, 0); // Mirror to left
-                }
-
+                obstacle.item.SetActive(true);
             }
         }
     }
 
     void RemoveObstacles()
     {
-        foreach (var obstacleData in obstacleContainers)
+        foreach (var obstacle in obstacles)
         {
-            Transform containerTransform = obstacleData.Item1;
-
-            if (Vector3.Distance(playerTransform.position, containerTransform.position) >= generateDistance + removeDistance)
+            if (Vector3.Distance(playerTransform.position, obstacle.container.transform.position) >= generateDistance + removeDistance)
             {
-                if (containerTransform.childCount > 0)
-                {
-                    Destroy(containerTransform.GetChild(0).gameObject);
-                }
+                obstacle.item.SetActive(false);
             }
+        }
+    }
+
+    void GetColliderSize(GameObject obstacleInstance)
+    {
+        CapsuleCollider2D capsuleCollider2D = obstacleInstance.GetComponentInChildren<CapsuleCollider2D>();
+        if (capsuleCollider2D != null)
+        {
+            float width = capsuleCollider2D.size.x; // Width of the collider
+            float height = capsuleCollider2D.size.y; // Height of the collider
+
+            // Log the values
+            Debug.Log("Width: " + width + ", Height: " + height);
+        }
+        else
+        {
+            Debug.LogError("CapsuleCollider2D not found on the child object.");
         }
     }
 }
